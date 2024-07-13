8c8bb94502239231731d638d334a5db151c2d0a1
juan.izquierdo <juan.izquierdo@rappi.com>
2024-07-12 22:00:19 -0500

    create project

---
+++ src/main/java/com/banco_financiera/BancoFinancieraApplication.java
@@ -0,0 +1,13 @@
+package com.banco_financiera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoFinancieraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoFinancieraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banco_financiera/BancoFinancieraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco_financiera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoFinancieraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
